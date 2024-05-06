a68dd605bfc954ea8d28e5f0a4fce36bccc68ebb
aucontraire <ocontreras.sf@gmail.com>
2024-05-06 16:42:18 -0700

    Add country level cities to mapbox

--- src/main/java/com/mm/mayhem/controller/CountryController.java
+++ src/main/java/com/mm/mayhem/controller/CountryController.java
@@ -1,10 +1,12 @@
 package com.mm.mayhem.controller;
 
+import com.mm.mayhem.model.db.geo.City;
 import com.mm.mayhem.model.db.geo.Country;
 import com.mm.mayhem.model.db.geo.StateRegion;
 import com.mm.mayhem.service.CountryService;
 import com.mm.mayhem.service.StateRegionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class CountryController {
     private final CountryService countryService;
+    private final Environment env;
 
     @Autowired
-    public CountryController(CountryService countryService) {
+    public CountryController(CountryService countryService, Environment env) {
         this.countryService = countryService;
+        this.env = env;
     }
 
     @GetMapping("/countries")
@@ -31,9 +36,13 @@ public class CountryController {
 
     @GetMapping("/countries/{id}")
     public String getStateRegionsByCountry(@PathVariable Long id, Model model) {
+        String mapboxAccessToken = env.getProperty("mayhem.mapbox.api.token");
+        model.addAttribute("mapboxAccessToken", mapboxAccessToken);
         Optional<Country> countryOptional = countryService.getCountryById(id);
         if (countryOptional.isPresent()) {
             Country country = countryOptional.get();
+            List<City> cities = country.getStateRegions().stream().flatMap(sr -> sr.getCities().stream()).collect(Collectors.toList());
+            model.addAttribute("cities", cities);
             model.addAttribute("country", country);
         }
         return "stateregions";
